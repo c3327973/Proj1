16159493fe155337e139f407d7e817723012392f
c3327973 <49221630+c3327973@users.noreply.github.com>
2019-04-11 18:00:54 +1000

    Update rotationcypher.c

--- rotationcypher.c
+++ rotationcypher.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int encryption(char str);
+char *encryption(char str[], int key);
 
 int main(void)
 {
-    char letters[1000];
-    char e; // temp storage
-    char c;                 // will store the vaules of each letter of the array to get changed
+    char letters[1000]; // stores the message from the console 
+    char encrypted_message[1000]; // this does nothing but changes things
+    int key;
+    printf("give me a message\n");
     scanf("%[^\n]s\n", letters); // take a user input 
     printf("%s\n", letters); // check what scanf stored in letters
-    int length;
-    length = strlen(letters);
-    for(int i=0 ; i< length; i++){
-        if ( letters[i] != 32 ){
-            e = letters[i] - 97;
-            c = ((e + 25)%26);
-            c = c + 97;
-    }
-        else {
-            c = letters[i];
-        }
-        printf("%c", c);
-    }
-    printf("\n");
-    printf("%s\n", letters); // compart the code to the original input
+    printf("give me a key\n");
+    scanf("%d\n", &key);
+    printf("%s\n", encryption(letters, key)); // compair the code to the original input
     return 0;
 }
 
-int encryption(char str){
-    
+char *encryption(char str[], int key){
+    int length;
+    char lowercase; // storage of letters
+    char uppercase;
+    char encrypt[1000];               // will store the vaules of each letter of the array to get changed
+    length = strlen(str);
+    for(int i=0 ; i< length; i++){
+        if ( str[i] != 32 ){
+            if ( str[i] < 91 ){
+                uppercase = str[i] - 65;
+                encrypt[i] = ((uppercase + key)%26);
+//                if (encrypt[i] < 0)
+//                    encrypt[i] = encrypt[i] + 26;
+                encrypt[i] = encrypt[i] + 65; 
+            }
+            else if ( 96 < str[i] )
+                lowercase = str[i] - 97;
+                encrypt[i] = ((lowercase + key)%26);
+                if (encrypt[i] < 0)
+                    encrypt[i] = encrypt[i]+26;
+                encrypt[i] = encrypt[i] + 97;
+            }
+        else {
+            encrypt[i] = str[i];
+        }
+    }
+    str = encrypt;
+    return str;
 }
